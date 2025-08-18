7afaeb51b73f8b57abc76ef260f71c34cb4d22fc
Wale <onafesowale@gmail.com>
2025-08-18 15:33:49 +0100

    init

---
+++ src/main/java/com/app/JuberEats/JuberEatsApplication.java
@@ -0,0 +1,13 @@
+package com.app.JuberEats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuberEatsApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(JuberEatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/JuberEats/JuberEatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.JuberEats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuberEatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
